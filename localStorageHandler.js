baacf9eb583ba99575b5494c8806e7fda4822190
SteffyCodes <stefftj29@gmail.com>
2024-10-31 16:18:37 +0100

    Started modularizing storageHandler function

--- localStorageHandler.js
+++ localStorageHandler.js
@@ -1,11 +1,21 @@
 // LOCAL CLIENT STORAGE HANDLER
 
 const storageHandler = {
-    listItem: null, // Placeholder value for the variable
     handleClick: function(event) {
-        taskItem = this.newTask // Accesses the current instance of newTask from coreTaskList.js
-        taskTitle = this.taskLabel
+        let storeTaskItem = this.newTask; // Accesses the current instance of newTask from coreTaskList.js
+        let storeTaskID = this.taskID;
+
+        localStorage.setItem(storeTaskID, storeTaskItem);
+
+        while (localStorage.getItem(storeTaskID !== null)) { //  "while" is good for looping logics and not conditional logics where you might use "if" 
+            storeTaskID++                                                                       // This way of error handling works good, but the order of tasks might end up different than original
+                                        // localStorage will return NULL if the key (storeTaskID) is duplicate. 
+        }
         
         
     }
+}
+
+window.onload = function() {
+    // Loading handler will go here 
 }
