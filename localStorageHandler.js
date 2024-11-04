04e93600f83a320521eba20b5f9c9251aec5182c
BytesFromStella <stefftj29@gmail.com>
2024-11-04 18:02:53 +0100

    Refactored saveTask to allow appending data. Completed functionality of the localStorage save/load features

--- localStorageHandler.js
+++ localStorageHandler.js
@@ -3,14 +3,18 @@
 const storageHandler = {
     saveTask: function(taskID, taskData) { // Modularized this function to make it reusable
         console.log("Task successfully saved to localStorage");
-        localStorage.setItem(taskID, JSON.stringify(taskData)) // Saving it as a JSON will make it alot easier to get the key-value pairs
+       
+        const existingTask = JSON.parse(localStorage.getItem(taskID)) || {}; // Add the new data or add an empty entry
+        const newTaskData = { ...existingTask, ...taskData }; // Allows you to update existing entries
+        
+        localStorage.setItem(taskID, JSON.stringify(newTaskData)) // Saving it as a JSON will make it alot easier to get the key-value pairs
     }, // In JS we add a comma to seperate 2 or more functions within an object literal (key-value pairs), except the last one
 
     loadAllTasks: function() {
-        var tasks = [];
         const insertToTarget = document.getElementById("taskList");
         insertToTarget.innerHTML = ""; // Clearing the list to prevent duplicate entires
-        // I should use a for-loop instead of a conditional While, since I'll be getting every item in localStorage anyways
+       
+        // Use a for-loop instead of a conditional While, since I'll be getting every item in localStorage anyways
         for (let count = 0; count < localStorage.length; count++) { // Iterates over all the data from the total length of the localStorage list
             var key = localStorage.key(count);
             const taskData = JSON.parse(localStorage.getItem(key));
@@ -23,10 +27,11 @@ const storageHandler = {
 
     generateUniqueID: function() { 
         console.log("Unique ID generated");
-        let counter = 0;
+        let counter = 1;
         let taskID;
         do {
             taskID = `ID-${counter}`; // prints "ID" along with the taskID number right after, stringified
+            console.log(taskID);
             counter++
         }  while (localStorage.getItem(taskID) ||  document.getElementById(taskID) !== null) { //  "while" is good for looping logics and not conditional logics where you might use "if" 
                                                                  // This way of error handling works good, but the order of tasks might end up different than original
@@ -35,6 +40,7 @@ const storageHandler = {
     },
 
    deleteTask: function(targetID) {
+        console.log(taskID);
         localStorage.removeItem(targetID); // No need for conditional statements here. If it can't delete the item, it should throw an error
    }
 }
