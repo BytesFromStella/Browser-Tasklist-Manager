41719304bca78a9f55dc5e7914529e57241ffc0d
BytesFromStella <steffentvenningj@gmail.com>
2025-01-12 05:54:14 +0100

    Refactored deadline input handling and improved visual cues; fixed bug with completion status for tasks with zero ID.

--- coreTaskList.js
+++ coreTaskList.js
@@ -13,6 +13,8 @@ const deadlineDatePointer = document.getElementById("deadlineDateField");
 const deadlineTimePointer = document.getElementById("deadlineTimeField");
 let deadlineInput = [deadlineDatePointer, deadlineTimePointer];
 let currentTimeDate = new Date();
+
+let completedState = 0; // 0 is false, 1 is true. This is used for the intervals on the deadline timer.
     
 //// TODO: Export and refactor to deadlineTimer.js
 function parseDeadline(deadlineInput) {
@@ -23,8 +25,7 @@ function parseDeadline(deadlineInput) {
     const deadlineTime = deadlineTimePointer.value || deadlineInput[1]?.value || false; // Adding a ? creates a null-safe access to the variable
     
     try {console.log("Date and time pointer values: "+deadlineDatePointer.value+deadlineTimePointer.value)} catch {console.log("Date and time pointer values: "+deadlineDatePointer+deadlineTimePointer)};
-    
-    // if(!deadlineInput||deadlineInput=="1970-01-01T00:00:00Z" ||deadlineInput=="1970-01-01T00:00:00"||deadlineInput==undefined||deadlineInput==0||deadlineInput==false) {console.log("falsey input date");return false; };
+
     // CHECKING for existing pre-parsed date object.
     if (deadlineInput instanceof Date) {console.warn("Date object detected. Stopping parsing");return deadlineInput}
     console.log("If-statement passed, pre-existing date object not detected");
@@ -37,14 +38,13 @@ function parseDeadline(deadlineInput) {
     (deadlineDate && !deadlineTime) ? new Date(deadlineDate+"T"+"23:59:59") :
     // If the date isn't provided, we need to zero-pad the date numbers. We only get a single digit returned for month and date (1:12:2024 as an example). Got chatGPT to write the syntax, but in theory it's a bunch of conditional operators using .length to check the digits.
     (!deadlineDate && deadlineTime) ? new Date(`${currentTimeDate.getFullYear()}-${String(currentTimeDate.getMonth() + 1).padStart(2, '0')}-${String(currentTimeDate.getDate()).padStart(2, '0')}T${deadlineTime}`) :
-    false; // Default fallback value
+    "1970-01-01T00:00:00.000Z"; // Default fallback value
     console.log(output);
     return output;
 }
 
 
-function createTaskElement(taskID, title, description, completed, deadline, priority) {
-    let completedState = 0; // 0 is false, 1 is true. This is used for the intervals on the deadline timer.
+function createTaskElement(taskID, title, description, completed, deadline, completedState, priority) {
     console.log("Creating new DOM object");
     
     // Creating each HTML object to append to the HTML document
@@ -57,6 +57,18 @@ function createTaskElement(taskID, title, description, completed, deadline, prio
     titleField.className = "taskPoint";
     titleField.textContent = title;
     newTask.appendChild(titleField);
+
+    // Priority
+    const priorityField = document.createElement("div");
+    const priorityBox = document.createElement("div");
+    priorityBox.className = "priorityBox";
+    priorityField.textContent = priority;
+    priorityField.className = "priority";
+
+    priorityField.appendChild(priorityBox); 
+    priorityBox.style.backgroundColor = priority;
+
+    newTask.appendChild(priorityField);
     
     // Description
     const descriptionField = document.createElement("p");
@@ -64,13 +76,27 @@ function createTaskElement(taskID, title, description, completed, deadline, prio
     descriptionField.className = "description"
     newTask.appendChild(descriptionField);
     
-    // Deadline field  
-    let deadlineParsed = new Date(parseDeadline(deadline));
+    // Deadline date and countdown field  
+    let deadlineParsed = parseDeadline(deadline);
     console.log(deadlineParsed);
     const deadlineField = document.createElement("div")
-    deadlineField.class = "deadline"
+    deadlineField.className = "deadline"<
     newTask.appendChild(deadlineField);
-    
+
+    let deadlineDateandTime = document.createElement("p");
+    deadlineDateandTime.id = "deadlineDateandTime";
+    deadlineDateandTime.className = "deadlineDateandTime";
+    if (deadlineParsed != "1970-01-01T00:00:00.000Z") {
+        deadlineDateandTime.innerHTML = 
+            "Deadline: "+"<br>"+
+            String(deadlineParsed.getMonth()+1).padStart(2, "0")+"."+
+            String(deadlineParsed.getDate()).padStart(2, "0")+"."+
+            String(deadlineParsed.getFullYear()) + " | "+
+            String(deadlineParsed.getHours()).padStart(2, "0")+":"+
+            String(deadlineParsed.getMinutes()).padStart(2, "0")+"<br>"
+    }
+    newTask.appendChild(deadlineDateandTime);
+
     // Checkbox
     const newCheckbox = document.createElement("input");
     newCheckbox.type = "checkbox";
@@ -89,42 +115,32 @@ function createTaskElement(taskID, title, description, completed, deadline, prio
     deleteButton.addEventListener("click", function() {
         newTask.remove();
         storageHandler.deleteTask(taskID);
-        clearInterval(newTask.countdown); ;
+        clearInterval(newTask.countdown);
     })
-    
+
     function startCountdown(deadlineParsed, deadlineField) {
         newTask.countdown = setInterval(() => {
             now = new Date();
             let secLeft = deadlineHandler.differenceInSeconds(deadlineParsed, now);
             // If the deadline isn't expired, it'll show the time left.
             if (secLeft >= 0) {
-                deadlineField.innerHTML = 
-                String(deadlineParsed.getMonth()+1).padStart(2, "0")+"."+
-                String(deadlineParsed.getDate()).padStart(2, "0")+"."+
-                String(deadlineParsed.getFullYear()) + " | "+
-                String(deadlineParsed.getHours()).padStart(2, "0")+":"+
-                String(deadlineParsed.getMinutes()).padStart(2, "0")+"<br>"+
+                deadlineField.innerHTML =
                 String(Math.trunc(deadlineHandler.differenceInDays(deadlineParsed, now)))+"d:"+
-                String(Math.trunc(deadlineHandler.differenceInHours(deadlineParsed, now))).padStart(2, "0")+"h:"+
+                String(Math.trunc(deadlineHandler.differenceInHours(deadlineParsed, now) % 24)).padStart(2, "0")+"h:"+
                 String(Math.floor(deadlineHandler.differenceInMinutes(deadlineParsed, now) % 60)).padStart(2, "0")+"m:"+ //TODO: Rewrite this into a dynamic timer.
                 String(Math.floor(deadlineHandler.differenceInSeconds(deadlineParsed, now) % 60)).padStart(2, "0")+"s left";
             }
             // If the deadline is expired, it'll show the time it's been expired for.
             else if (secLeft < 0){
                 deadlineField.innerHTML = 
-                String(deadlineParsed.getMonth()+1).padStart(2, "0")+"."+
-                String(deadlineParsed.getDate()).padStart(2, "0")+"."+
-                String(deadlineParsed.getFullYear()) + " | "+
-                String(deadlineParsed.getHours()).padStart(2, "0")+":"+
-                String(deadlineParsed.getMinutes()).padStart(2, "0")+"<br>"+
                 "Expired by:<br>"+
                 String(Math.trunc(Math.abs(deadlineHandler.differenceInDays(deadlineParsed, now))))+"d:"+
-                String(Math.trunc(Math.abs(deadlineHandler.differenceInHours(deadlineParsed, now)))).padStart(2, "0")+"h:"+
+                String(Math.trunc(Math.abs(deadlineHandler.differenceInHours(deadlineParsed, now) % 24))).padStart(2, "0")+"h:"+
                 String(Math.floor(Math.abs(deadlineHandler.differenceInMinutes(deadlineParsed, now) % 60))).padStart(2, "0")+"m:"+ //TODO: Rewrite this into a dynamic timer.
                 String(Math.floor(Math.abs(deadlineHandler.differenceInSeconds(deadlineParsed, now) % 60))).padStart(2, "0")+"s";
             }
             else{
-                throw new Error("failure initializing countdown interval");
+                throw new Error("Failed to initialize countdown interval");
                 
             }
             // Updating task item borders
@@ -154,23 +170,38 @@ function createTaskElement(taskID, title, description, completed, deadline, prio
     
     function completedStateCheck(completedState, completed, taskID, deadlineParsed) {
         switch (completedState) {
-            case 0: 
+            case 0: // Not completed, before deadline
                 checkboxStylingUncomplete(taskID, newTask, completed);
                 deadlineHandler.timelimitApplied(newTask, deadlineParsed, completed); // This will instantly refresh the border style to the uncompleted style
                 startCountdown(deadlineParsed, deadlineField);
+                storageHandler.saveTask(taskID, {completionDate: false});
                 console.log("Task not completed. Applying intervals");
                 break;
-            case 1:
-                checkboxStylingComplete(taskID, newTask, completed);
-                stopCountdown(newTask); 
-                deadlineHandler.timelimitApplied(newTask, deadlineParsed, completed); // This will instantly refresh the border style to the completed style
-                localStoragehandler.saveTask(taskID, {completionDate: completed.checked});
+            case 1: // Not completed, after deadline
+                checkboxStylingUncomplete(taskID, newTask, completed);
+                deadlineHandler.timelimitApplied(newTask, deadlineParsed, completed); // This will instantly refresh the border style to the completed style          
+                storageHandler.saveTask(taskID, {completionDate: false});
+                startCountdown(deadlineParsed, deadlineField);
                 console.log("Task completed. Interval not applied!");
                 break;
             case 2: // Completed before deadline
+                checkboxStylingComplete(taskID, newTask, completed, completedState);
+                storageHandler.saveTask(taskID, {completed: completed.checked, completionDate: new Date()});
+                stopCountdown(newTask); 
+                break;
             case 3: // Completed after deadline
-
-            case 99:
+                checkboxStylingComplete(taskID, newTask, completed, completedState);
+                storageHandler.saveTask(taskID, {completed: completed.checked, completionDate: new Date()});
+                stopCountdown(newTask); 
+                break;
+            case 4: // No deadline set
+                checkboxStylingUncomplete(taskID, newTask, completed, completedState);
+                break;
+            case 5:
+                checkboxStylingComplete(taskID, newTask, completed, completedState);
+                storageHandler.saveTask(taskID, {completed: completed.checked, completionDate: new Date()});
+                break;
+            case 99: // Fallback value 
                 console.log("Invalid, false or default date object. Interval not applied!");
                 break;
             default: 
@@ -181,8 +212,24 @@ function createTaskElement(taskID, title, description, completed, deadline, prio
 
     function checkboxEvent() {
         // Advanced condition check based on completion timing 
-        completedState =  (newCheckbox.checked == false) ? 0 : (newCheckbox.checked == true) ? 1 : 2;
-        console.log("Checkbox event triggered. Completed state: " + completedState); 
+        currentTimeDate = new Date(); // Refresh the current time for the interval
+        if (!deadlineParsed || deadlineParsed === "1970-01-01T00:00:00.000Z") { 
+            completedState = 4;} // No deadline set
+        else if (newCheckbox.checked == false 
+            && deadlineParsed > currentTimeDate) 
+            {completedState = 0;} // Not completed, before deadline
+        else if (newCheckbox.checked == false 
+            && deadlineParsed < currentTimeDate) 
+            {completedState = 1;} // Not completed, after deadline
+        else if (newCheckbox.checked == true 
+            && deadlineParsed > currentTimeDate) 
+            {completedState = 2;} // Completed, before deadline
+        else if (newCheckbox.checked == true 
+            && deadlineParsed < currentTimeDate) 
+            {completedState = 3;} // Completed, after deadline
+        else {completedState = 99;} // Default value for error handling
+        
+        console.log("Checkbox event triggered. Completion state: " + completedState); 
         completedStateCheck(completedState, newCheckbox, taskID, deadlineParsed);}; // Copilot is a great tool for writing code, but it's not perfect. It's important to understand the code you're writing. (That was written by copilot). Btw, this is also a ternary operator. If the checkbox is checked, it'll return 1, otherwise 0.
         
         // This will work because the HTML objects will return FALSE if nothing is inputted. When loading the application, the form is empty by default anyways, so this will work.
@@ -197,17 +244,19 @@ function handleClick(event) {
     console.log("Running handleClick");
     const taskID = storageHandler.generateUniqueID();
     const title = submitText.value || "Task Title";
+    const priority = document.getElementById("priorityField").value || "medium";
     const description = descriptionText.value || "";  // Default to an empty description
     const completed = false;
     const deadlineOutput = parseDeadline(deadlineInput) || false;
 
 
-    const newTask = createTaskElement(taskID, title, description, completed, deadlineOutput);
+    const newTask = createTaskElement(taskID, title, description, completed, deadlineOutput, completedState, priority);
     document.getElementById("taskList").appendChild(newTask);
 
     // Clear both input fields for additional entries
     submitText.value = "";
     descriptionText.value = "";
+    priorityField.value = "medium";
     deadlineDatePointer.value = "";
     deadlineTimePointer.value = "";
 
@@ -217,7 +266,8 @@ function handleClick(event) {
         title: title,
         description: description,
         completed: completed,
-        deadline: deadlineOutput
+        deadline: deadlineOutput,
+        priority: priority
         /* Additional data points from e.g localStorageHandler can pass new key:value pairs into the localStorage JSON */
     })
 }
@@ -231,11 +281,30 @@ window.onload = function deleteAllTasks() {
 } 
 
 addTask.addEventListener("click", handleClick);  // Adding this AFTER the function so it doesn't call something undefined.
-submitText.addEventListener("keydown", function(event) {
+descriptionText.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         handleClick();
         event.preventDefault(); // Prevents the browser from treating this as a web submission form
     }
 });
 
+// Quick little feature added because it's hard to look at during the night. It has persistent storage as well.
+document.addEventListener("DOMContentLoaded", function() {
+    const darkmodeToggle = document.getElementById("darkmodeToggle");
 
+    // Check if dark mode is already enabled in local storage
+    if (localStorage.getItem("darkmode") === "enabled") {
+        document.body.classList.add("darkmode");
+        darkmodeToggle.checked = true;
+    }
+
+    darkmodeToggle.addEventListener("change", function() {
+        if (darkmodeToggle.checked) {
+            document.body.classList.add("darkmode");
+            localStorage.setItem("darkmode", "enabled");
+        } else {
+            document.body.classList.remove("darkmode");
+            localStorage.setItem("darkmode", "disabled");
+        }
+    });
+});
