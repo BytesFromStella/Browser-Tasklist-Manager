51b08e0462273e3feb57f2ac08a7f1ab6122a7eb
BytesFromStella <steffentvenningj@gmail.com>
2025-01-31 09:47:31 +0100

    Centralized the navigation bar into a seperate JS-file; Ensures that the nav-bar is identical on all pages, and a JS is the closest thing to vanilla JS I could come up with

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+    <li>Fuck it, dark mode. Check this box to enable it: <br><input type="checkbox" id="darkmodeToggle"> </li>
+    <li><a href="landingPage.html">Welcome</a></li>
+    <li><a href="main.html">Task manager</a></li>
+        `;
